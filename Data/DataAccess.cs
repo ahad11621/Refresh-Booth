a6d914b536309a3b6849e3167251d8868b9fdcfb
ahad11621 <63227929+ahad11621@users.noreply.github.com>
2021-11-15 23:45:07 +0600

    Folder structure created

---
+++ Data/DataAccess.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using MySql.Data.MySqlClient;
+
+namespace ATM_System.Data
+{
+    class DataAccess
+    {
+        static MySqlConnection connection;
+        static MySqlCommand command;
+
+        public DataAccess()
+        {
+            connection = new MySqlConnection("datasource = localhost;username = root; password=; database = atm-system");
+            connection.Open();
+        }
+
+        public MySqlDataReader GetData(string sql)
+        {
+            command = new MySqlCommand(sql, connection);
+            return command.ExecuteReader();
+        }
+
+        public int ExecuteQuery(string sql)
+        {
+            command = new MySqlCommand(sql, connection);
+            return command.ExecuteNonQuery();
+        }
+
+        public void Dispose()
+        {
+            connection.Close();
+        }
+        ~DataAccess()
+        {
+            connection.Close();
+        }
+    }
+}
--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -29,13 +29,70 @@ namespace Refresh_Booth
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.panel2 = new System.Windows.Forms.Panel();
+            this.panel3 = new System.Windows.Forms.Panel();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.panel4 = new System.Windows.Forms.Panel();
+            this.SuspendLayout();
+            // 
+            // panel2
+            // 
+            this.panel2.BackColor = System.Drawing.Color.DarkOrange;
+            this.panel2.Dock = System.Windows.Forms.DockStyle.Bottom;
+            this.panel2.Location = new System.Drawing.Point(0, 542);
+            this.panel2.Name = "panel2";
+            this.panel2.Size = new System.Drawing.Size(700, 58);
+            this.panel2.TabIndex = 1;
+            // 
+            // panel3
+            // 
+            this.panel3.BackColor = System.Drawing.Color.DarkOrange;
+            this.panel3.Dock = System.Windows.Forms.DockStyle.Top;
+            this.panel3.Location = new System.Drawing.Point(0, 0);
+            this.panel3.Name = "panel3";
+            this.panel3.Size = new System.Drawing.Size(700, 96);
+            this.panel3.TabIndex = 2;
+            // 
+            // panel1
+            // 
+            this.panel1.BackColor = System.Drawing.Color.DarkOrange;
+            this.panel1.Dock = System.Windows.Forms.DockStyle.Left;
+            this.panel1.Location = new System.Drawing.Point(0, 96);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(10, 446);
+            this.panel1.TabIndex = 3;
+            // 
+            // panel4
+            // 
+            this.panel4.BackColor = System.Drawing.Color.DarkOrange;
+            this.panel4.Dock = System.Windows.Forms.DockStyle.Right;
+            this.panel4.Location = new System.Drawing.Point(690, 96);
+            this.panel4.Name = "panel4";
+            this.panel4.Size = new System.Drawing.Size(10, 446);
+            this.panel4.TabIndex = 4;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(700, 600);
+            this.Controls.Add(this.panel4);
+            this.Controls.Add(this.panel1);
+            this.Controls.Add(this.panel3);
+            this.Controls.Add(this.panel2);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
             this.Text = "Form1";
+            this.ResumeLayout(false);
+
         }
 
         #endregion
+        private System.Windows.Forms.Panel panel2;
+        private System.Windows.Forms.Panel panel3;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Panel panel4;
     }
 }
 
